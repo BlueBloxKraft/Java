88b035cc71e36a80fc6af585be33a1ef8b63ace0
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2022-03-24 15:12:11 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+	static Scanner s = new Scanner(System.in);
+	static String[] eq = {
+			"add",
+			"subtract",
+			"multiply",
+			"divide"
+	};
+	
+	public static void main(String[] args) {
+		startup();
+		calculate();
+	}
+	
+	private static void startup() {
+		System.out.println("These are the possible equations you can use:");
+		for(int i = 0; i < eq.length; i++) {
+			System.out.println(eq[i]);
+		}
+	}
+	
+	private static void calculate() {
+		
+		String success = "Your answer is: ";
+		String str = getUserString();
+		
+		System.out.println("Give me the first number!");
+		int a = s.nextInt();
+		
+		System.out.println("Give me the second number!");
+		int b = s.nextInt();
+		
+		
+		if(str.equals(eq[0])) {
+			System.out.println(success + (a + b));
+		}
+		
+		if(str.equals(eq[1])) {
+			System.out.println(success + (a - b));
+		}
+		
+		if(str.equals(eq[2])) {
+			System.out.println(success + (a * b));
+		}
+		
+		if(str.equals(eq[3])) {
+			System.out.println(success + (a / b));
+		}
+		
+	}
+	
+	private static String getUserString() {
+		String somename = null;
+		String str = s.nextLine();
+		
+		for(int i = 0; i < eq.length; i++) {
+			if(str.equals(eq[i])) {
+				somename = eq[i];
+				System.out.println("Give me two numbers to " + eq[i] + "!");
+				System.out.println("-------------------------------------");
+			} else {
+				System.out.println("Oh no! Something went wrong. Maybe it is a typo?");
+				startup();
+			}
+		}
+		return somename;
+	}
+	
+}
